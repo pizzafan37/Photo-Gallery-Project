58dac5a9a2d1d355ec08109715b51dfcb35132bf
Zachary Bland <67700224+pizzafan37@users.noreply.github.com>
2020-12-16 13:14:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+    
 let characters = ["https://static.wikia.nocookie.net/nintendo/images/d/d8/New_Super_Mario_Bros._U_Deluxe_-_Mario_01.png/revision/latest?cb=20181226204245&path-prefix=en", "https://vignette.wikia.nocookie.net/sonicpokemon/images/7/77/Pikachu.png/revision/latest?cb=20200831092023", "https://static.wikia.nocookie.net/sonic/images/2/2d/TSR_Sonic.png/revision/latest?cb=20191020043348"];
 
 var store;
@@ -13,3 +15,4 @@ characters.forEach(function(character, image){
 });
 
 characters.push(".image-url");
+    });
