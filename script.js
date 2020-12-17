eeffc99c8a0884f6097153db76416ea32b6e3005
Zachary Bland <67700224+pizzafan37@users.noreply.github.com>
2020-12-17 12:35:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-let characters = ["https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png", "https://vignette.wikia.nocookie.net/sonicpokemon/images/7/77/Pikachu.png/revision/latest?cb=20200831092023", "https://static.hollywoodreporter.com/sites/default/files/2017/07/sonic-928x523.jpg"];
+let characters = ["https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png", "https://vignette.wikia.nocookie.net/sonicpokemon/images/7/77/Pikachu.png/revision/latest?cb=20200831092023", "https://static.wikia.nocookie.net/disneyfanon/images/b/bd/5b451408e63c51d8166f981454baf615.png"];
 
 var store;
 
